11385f7076868eed637d41cecb207767340baf37
hikida <ponponda0103@gmail.com>
2024-09-24 20:55:02 +0900

    いったんリセット

--- src/App.js
+++ src/App.js
@@ -8,11 +8,27 @@ function App(){
   useEffect(()=>{
     // ページがロードされた時にAPIを呼び出す
 
-    fetch('http://localhost:8080')  // GoのAPIエンドポイント
+    fetch('http://backend:8080')  // GoのAPIエンドポイント
       .then((response) => response.json())
       .then((jsonData) => setData(jsonData))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);  // 空の配列を渡すことで、初回のみ実行される
 
+    // データがまだ取得されていない場合はローディング状態を表示
+    if (!data) {
+      return <div>Loading...</div>;
+    }
 
+      // 取得したデータを表示
+    return (
+      <div>
+        <h1>ユーザー情報</h1>
+        <p><strong>ID:</strong> {data._id}</p>
+        <p><strong>名前:</strong> {data.git_name}</p>
+        <p><strong>メール:</strong> {data.mail}</p>
+        <p><strong>時間:</strong> {data.time}</p>
+      </div>
+    );
 }
+
+export default App;
