44cc3bd5de65d76371cda16f3d528e32f791e82b
hikida <ponponda0103@gmail.com>
2024-11-07 22:26:55 +0900

    change fetchURL that getting to .env

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -2,18 +2,33 @@ import { GoogleLogin } from '@react-oauth/google';
 import {authenticateUser} from './api/authApi';
 
 const LoginButton = () => {
-  const handleSuccess = async(response) => {
+    const[userInfo,setUserInfo] = useState(null); //UserInfo管理
+
+    const handleSuccess = async(response) => {
     const token = response.credential;
     console.log("IDトークン:", token); //dbg用、後で消す
 
     try{
         const data = await authenticateUser(token);
         console.log("バックエンドからの応答",data);
+        setUserInfo(data);
     }catch(error)
     {
         console.error("エラー:",error);
     }
   };
+
+  if (userInfo) 
+    {
+        // userInfoが存在する場合は、ユーザー情報を表示
+        return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>Welcome, {userInfo.userName}!</h1>
+            <p>Email: {userInfo.email}</p>
+        </div>
+        );
+    }
+
 
 // <GoogleLogin>はGoogleログインボタンを表示し、ユーザーがログインした後にonsuccsイベントで認証情報を取得する
   return (
